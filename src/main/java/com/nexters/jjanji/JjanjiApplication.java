469fbc6746c21e83e27e9ac95c23af4874b34209
JinuCheon <woojin8787@gmail.com>
2023-07-08 00:05:07 +0900

    init: 프로젝트 환경 세팅

---
+++ src/main/java/com/nexters/jjanji/JjanjiApplication.java
@@ -0,0 +1,13 @@
+package com.nexters.jjanji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjanjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjanjiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexters/jjanji/JjanjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexters.jjanji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjanjiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
