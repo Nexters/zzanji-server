1938fa6ad83f849ada6d6620b36194b025361532
JinuCheon <woojin8787@gmail.com>
2023-07-12 18:44:00 +0900

    feat: 다음주 챌린지 참가 요청 api 구현

--- src/main/java/com/nexters/jjanji/domain/challenge/dto/request/ParticipateRequestDto.java
+++ src/main/java/com/nexters/jjanji/domain/challenge/dto/request/ParticipateRequestDto.java
@@ -1,10 +1,19 @@
 package com.nexters.jjanji.domain.challenge.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
+@NoArgsConstructor
 public class ParticipateRequestDto {
+
+    @NotNull
     private Long goalAmount;
+
+    @Builder
+    public ParticipateRequestDto(Long goalAmount) {
+        this.goalAmount = goalAmount;
+    }
 }
--- src/main/java/com/nexters/jjanji/domain/challenge/presentation/ChallengeController.java
+++ src/main/java/com/nexters/jjanji/domain/challenge/presentation/ChallengeController.java
@@ -3,21 +3,24 @@ package com.nexters.jjanji.domain.challenge.presentation;
 import com.nexters.jjanji.domain.challenge.application.ChallengeService;
 import com.nexters.jjanji.domain.challenge.dto.request.ParticipateRequestDto;
 import com.nexters.jjanji.global.auth.MemberContext;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/challenge")
+@RequestMapping("/v1/challenge")
 @RequiredArgsConstructor
 public class ChallengeController {
 
     private final ChallengeService challengeService;
 
-    @RequestMapping("/participate")
-    public void participateNextChallenge(ParticipateRequestDto participateRequestDto) {
+    @PostMapping("/participate")
+    public void participateNextChallenge(@Valid @RequestBody ParticipateRequestDto participateRequestDto) {
         Long memberId = MemberContext.getContext();
+        System.out.println("participateRequestDto = " + participateRequestDto.getGoalAmount());
         challengeService.participateNextChallenge(memberId, participateRequestDto);
     }
 }
