62e7e21f016f9c5e0ac6db2d3146e7e1257cb3cc
JinuCheon <woojin8787@gmail.com>
2023-07-22 16:23:41 +0900

    refactor: 기존 예외처리 공통 exception으로 변경

---
+++ src/main/java/com/nexters/jjanji/global/exception/AlreadyParticipateException.java
@@ -0,0 +1,7 @@
+package com.nexters.jjanji.global.exception;
+
+public class AlreadyParticipateException extends BaseException {
+    public AlreadyParticipateException() {
+        super("이미 다음 챌린지에 참여중입니다.");
+    }
+}
---
+++ src/main/java/com/nexters/jjanji/global/exception/NotParticipateException.java
@@ -0,0 +1,7 @@
+package com.nexters.jjanji.global.exception;
+
+public class NotParticipateException extends BaseException {
+    public NotParticipateException() {
+        super("아직 챌린지에 참여하지 않았습니다.");
+    }
+}
