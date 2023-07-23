2bd6578f0fdcff7c3e527bcf14f7688a200e6289
BonSik-Koo <rnqhstlr2297@gmail.com>
2023-07-23 22:58:33 +0900

    test: 지출 내역 수정 비지니스 로직 단위 테스트 코드 작성

--- src/test/java/com/nexters/jjanji/domain/challenge/application/SpendingHistoryServiceTest.java
+++ src/test/java/com/nexters/jjanji/domain/challenge/application/SpendingHistoryServiceTest.java
@@ -1,13 +1,17 @@
 package com.nexters.jjanji.domain.challenge.application;
 
+import com.nexters.jjanji.domain.challenge.domain.Challenge;
 import com.nexters.jjanji.domain.challenge.domain.Plan;
 import com.nexters.jjanji.domain.challenge.domain.SpendingHistory;
+import com.nexters.jjanji.domain.challenge.domain.repository.ChallengeRepository;
 import com.nexters.jjanji.domain.challenge.domain.repository.PlanRepository;
 import com.nexters.jjanji.domain.challenge.domain.repository.SpendingHistoryRepository;
+import com.nexters.jjanji.domain.challenge.dto.request.SpendingEditDto;
 import com.nexters.jjanji.domain.challenge.dto.request.SpendingSaveDto;
 import com.nexters.jjanji.domain.challenge.dto.response.SpendingDetailResponse;
 import com.nexters.jjanji.domain.challenge.specification.PlanCategory;
 import com.nexters.jjanji.global.exception.PlanNotFoundException;
+import com.nexters.jjanji.global.exception.SpendingPeriodInvalidException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +21,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.SliceImpl;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,6 +38,7 @@ class SpendingHistoryServiceTest {
 
     @Mock PlanRepository planRepository;
     @Mock SpendingHistoryRepository spendingHistoryRepository;
+    @Mock ChallengeRepository challengeRepository;
     @InjectMocks SpendingHistoryService spendingHistoryService;
 
     @Test
@@ -102,4 +109,59 @@ class SpendingHistoryServiceTest {
         verify(spendingHistoryRepository, times(1)).findCursorSliceByPlan(plan, 1L, pageRequest);
     }
 
+    @Test
+    @DisplayName("지출 내역 수정에 성공하다.")
+    void editSpendingHistory(){
+        //given
+        final Challenge challenge = mock(Challenge.class);
+        final Plan plan = mock(Plan.class);
+        final SpendingHistory spending = mock(SpendingHistory.class);
+        final Long categorySpendAmount = 5000L;
+        final Long spendAmount = 3000L;
+
+        given(challengeRepository.findChallengeByPlanId(1L))
+                .willReturn(Optional.of(challenge));
+        given(planRepository.findById(1L))
+                .willReturn(Optional.of(plan));
+        given(spendingHistoryRepository.findById(1L))
+                .willReturn(Optional.of(spending));
+
+        given(challenge.isOpenedChallenge())
+                .willReturn(true);
+        given(plan.getCategorySpendAmount())
+                .willReturn(categorySpendAmount);
+        given(spending.getSpendAmount())
+                .willReturn(spendAmount);
+
+        final SpendingEditDto dto = new SpendingEditDto("엔제리너스", "부모님", 1000L);
+
+        //when
+        Long spendingId = spendingHistoryService.editSpendingHistory(1L, 1L, dto);
+
+        //then
+        verify(challengeRepository, times(1)).findChallengeByPlanId(1L);
+        verify(planRepository, times(1)).findById(1L);
+        verify(spendingHistoryRepository, times(1)).findById(1L);
+        verify(challenge, times(1)).isOpenedChallenge();
+        verify(plan,times(1)).getCategorySpendAmount();
+        verify(spending,times(1)).getSpendAmount();
+        verify(plan,times(1)).updateCategorySpendAmount(categorySpendAmount-spendAmount+dto.getSpendAmount());
+    }
+
+    @Test
+    @DisplayName("종료된 챌린지에 지출 내역 입력을 시도하다.")
+    void editSpendingHistory_finishChallenge(){
+        //given
+        final Challenge challenge = mock(Challenge.class);
+        final SpendingEditDto dto = mock(SpendingEditDto.class);
+
+        given(challengeRepository.findChallengeByPlanId(1L))
+                .willReturn(Optional.of(challenge));
+        given(challenge.isOpenedChallenge())
+                .willReturn(false);
+
+        //when & then
+        assertThatThrownBy(() -> spendingHistoryService.editSpendingHistory(1L, 1L, dto))
+                .isInstanceOf(SpendingPeriodInvalidException.class);
+    }
 }
