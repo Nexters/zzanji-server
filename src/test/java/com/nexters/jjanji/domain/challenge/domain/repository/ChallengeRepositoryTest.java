7bbdf7d9ebc4f85d6315c7249ef679f727b152a7
JinuCheon <woojin8787@gmail.com>
2023-08-27 02:16:40 +0900

    fix: 테스트 롤백용 transactional 적용

--- src/test/java/com/nexters/jjanji/domain/challenge/domain/repository/ChallengeRepositoryTest.java
+++ src/test/java/com/nexters/jjanji/domain/challenge/domain/repository/ChallengeRepositoryTest.java
@@ -21,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class ChallengeRepositoryTest {
     @Autowired ChallengeRepository challengeRepository;
     @Autowired MemberRepository memberRepository;
